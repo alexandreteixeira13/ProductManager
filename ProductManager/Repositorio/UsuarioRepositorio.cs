e6f8c67f35057b8162615c26f178f563a5607f6c
Alexandre <mikazittas@gmail.com>
2025-06-04 08:43:59 -0300

    Adicionando a funcao de obter usuario no usuariorepositorio

--- ProductManager/Repositorio/UsuarioRepositorio.cs
+++ ProductManager/Repositorio/UsuarioRepositorio.cs
@@ -1,9 +1,27 @@
-﻿using MySql.Data.MySqlClient;
-using System.Data;
+﻿using System.Data;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+using MySql.Data.MySqlClient;
+using ProductManager.Models;
 
 namespace ProductManager.Repositorio
 {
-    public class UsuarioRepositorio
+    public class UsuarioRepositorio(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public Usuario ObterUsuario(string email)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+
+                conexao.Open();
+
+                MySqlCommand cmd = new("SELECT * FROM Usuarios WHERE = @email", conexao);
+
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                using (MySqlDataReader)
+            }
+        }
     }
 }
