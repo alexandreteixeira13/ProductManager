8777f52c6ce72fd4de9b5ab17c87a7f3406694bb
Alexandre <mikazittas@gmail.com>
2025-06-04 08:19:45 -0300

    Criando as classes Usuario e Produto

---
+++ ProductManager/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductManager.Models
+{
+    public class Produto
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+         
+        public string descricao { get; set; }
+
+        public int preco { get; set; }
+
+        public int quantidade { get; set; }
+
+    }
+}
